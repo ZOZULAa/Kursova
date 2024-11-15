cc46289f5f5b0e1ec7cbc2deef8307a01b848450
ZOZULAa <zozula.yr@gmail.com>
2024-11-15 14:45:33 +0200

    feat: breeze auth

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DrugController;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/medicineList/search', 'App\Http\Controllers\MedicineListController@index')->name('medicineList');
-Route::get('/medicineList', 'App\Http\Controllers\MedicineListController@index')->name('medicineList.index');
-Route::post('/medicineList', 'App\Http\Controllers\MedicineListController@store')->name('medicineList.store');
-Route::get('/medicineList/{medicine}', 'App\Http\Controllers\MedicineListController@show')->name('medicineList.show');
-
-Route::get('/medicine/search', 'App\Http\Controllers\MedicineController@index')->name('medicine');
-Route::get('/medicine', 'App\Http\Controllers\MedicineController@index')->name('medicine.index');
-Route::get('/medicine/create', 'App\Http\Controllers\MedicineController@create')->name('medicine.create');
-Route::post('/medicine', 'App\Http\Controllers\MedicineController@store')->name('medicine.store');
-Route::get('/medicine/{medicine}', 'App\Http\Controllers\MedicineController@show')->name('medicine.show');
-Route::get('/medicine/{medicine}/edit', 'App\Http\Controllers\MedicineController@edit')->name('medicine.edit');
-Route::patch('/medicine/{medicine}', 'App\Http\Controllers\MedicineController@update')->name('medicine.update');
-Route::delete('/medicine/{medicine}', 'App\Http\Controllers\MedicineController@destroy')->name('medicine.delete');
-
-Route::get('/medicine/update', 'App\Http\Controllers\MedicineController@update');
-Route::get('/medicine/delete', 'App\Http\Controllers\MedicineController@delete');
-
-
+require __DIR__.'/auth.php';
